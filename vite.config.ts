4c116fef186e0dbf1e24238198bf66d7138bb66f
João Guilherme <joaoguilhermecarvalhonovaes@gmail.com>
2025-11-23 15:52:06 -0300

    viteconfigFixed

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  
-  // 👇 Adicione esta seção
-  preview: {
-    allowedHosts: [
-      'equilibrium-906663117168.us-central1.run.app/'
+  plugins: [react(), tailwindcss()],
+  preview: {
+    allowedHosts: [
+      'equilibrium-906663117168.us-central1.run.app'
     ]
-  }
+  }
 })
